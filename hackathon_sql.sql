60d34d088a327e48072650f0f54fce06ba2ed870
rasinha621 <119453940+rasinha621@users.noreply.github.com>
2024-03-07 12:01:18 -0500

    hackathon_sql.sql

--- hackathon_sql.sql
+++ hackathon_sql.sql
@@ -1,13 +1,15 @@
 SELECT 
-  id,
-  name,
-  category_name,
-  COUNT(*),
-  SUM(value) AS total_value,
-  MAX(timestamp) AS latest_timestamp
-FROM products p
-JOIN categories ON products.category_id = category_id 
-WHERE price > 50
+  voterbase_id,
+  vb_tsmart_first_name,
+  vb_tsmart_last_name
+  tmc_cell_phone,
+  ts_tsmart_ideology_score,
+  vb_vf_source_state,
+  ad_info,
+FROM prj-training-ao7c,voterfiles.voterfile_snapshot p
+JOIN prj-training-ao7c.data_mart.ads_data a 
+ON p.voterbase_id = a.voterbase_id
+where p.vb_vf_p2016 = 'D'
+where p.vb_voterbase_age in (50,51,52,53,54,55,56,57,58,59,60,61,62,63,64)
 ORDER BY latest_timestamp 
-GROUP BY id,
-LIMIT 10
+GROUP BY dvoterbase_id
